a968de8afedd800b5596e37a54f2991fcbe0fd24
ngoupatrick <ngounepatrick@gmail.com>
2023-10-10 14:52:35 +0000

    adding front

--- front_end/script.js
+++ front_end/script.js
@@ -1,37 +1,37 @@
-var apiUrl ="";
+//var apiUrl ="";
 
-// Load the JSON configuration
-fetch('conf/config.json')
-    .then(response => response.json())
-    .then(config => {
-        // Use the API URL from the configuration
-        apiUrl = config.apiUrl;
-        console.log(apiUrl); // Optional: Log the API URL for verification
-        
-    })
-    .catch(error => {
-        console.error('Error loading configuration:', error);
-    });
 
 function getDataFromAPI() {
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    displayData(data);
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        }
-		
+   // Load the JSON configuration
+   fetch('conf/config.json')
+      .then(response => response.json())
+      .then(config => {
+         // Use the API URL from the configuration
+         const apiUrl = config.apiUrl;
+         console.log(apiUrl); // Optional: Log the API URL for verification
+
+      })
+      .catch(error => {
+         console.error('Error loading configuration:', error);
+      });
+
+   fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+         displayData(data);
+      })
+      .catch(error => {
+         console.error('Error fetching data:', error);
+      });
+}
+
 function displayData(data) {
-            const resultContainer = document.getElementById('apiResult');
-            resultContainer.innerHTML = `
+   const resultContainer = document.getElementById('apiResult');
+   resultContainer.innerHTML = `
                 <p>User ID: ${data.userId}</p>
                 <p>Title: ${data.title}</p>
                 <p>Completed: ${data.completed ? 'Yes' : 'No'}</p>
             `;
-        }
+}
 // script.js
 document.getElementById('getDataButton').addEventListener('click', getDataFromAPI);
-
