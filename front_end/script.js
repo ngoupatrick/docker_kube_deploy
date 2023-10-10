86111e1cf19656e3bbd0388964fe1c466151f2b4
ngoupatrick <ngounepatrick@gmail.com>
2023-10-10 20:18:45 +0000

    adding front

--- front_end/script.js
+++ front_end/script.js
@@ -1,10 +1,10 @@
 //var apiUrl ="";
 
-
+/**
 function getDataFromAPI() {
    // Load the JSON configuration
-   //fetch('./conf/config.json')
-   fetch('http://localhost/conf/config.json')
+   fetch('conf/config.json')
+   //fetch('http://localhost/conf/config.json')
       .then(response => response.json())
       .then(config => {
          // Use the API URL from the configuration
@@ -25,14 +25,45 @@ function getDataFromAPI() {
       });
 
 
-}
-
+}**/
+/**
 function displayData(data) {
    const resultContainer = document.getElementById('apiResult');
    resultContainer.innerHTML = `
                 <p>User ID: ${data.userId}</p>
                 <p>Title: ${data.title}</p>
                 <p>Completed: ${data.completed ? 'Yes' : 'No'}</p>
+            `;
+}
+**/
+function getDataFromAPI() {
+   // Load the JSON configuration
+   fetch('config.json')
+   //fetch('http://localhost/conf/config.json')
+      .then(response => response.json())
+      .then(config => {
+         // Use the API URL from the configuration
+         const apiUrl = config.apiUrl;
+         console.log(apiUrl); // Optional: Log the API URL for verification
+         fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+               displayData(data);
+            })
+            .catch(error => {
+               console.error('Error fetching data:', error);
+            });
+
+      })
+      .catch(error => {
+         console.error('Error loading configuration:', error);
+      });
+}
+
+function displayData(data) {
+   const resultContainer = document.getElementById('apiResult');
+   resultContainer.innerHTML = `
+                <p>${data}</p>
             `;
 }
 // script.js
