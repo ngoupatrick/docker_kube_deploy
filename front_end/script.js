2fb2b5194e4b584db571e1e1e140ff1329491601
ngoupatrick <ngounepatrick@gmail.com>
2023-10-10 11:44:31 +0000

    adding front

--- front_end/script.js
+++ front_end/script.js
@@ -1,41 +1,34 @@
-// npm install js-yaml
+// script.js
+document.getElementById('getDataButton').addEventListener('click', getDataFromAPI);
 
-//import yaml from 'js-yaml';
-const yaml = require('js-yaml');
+// Load the JSON configuration
+fetch('config.json')
+    .then(response => response.json())
+    .then(config => {
+        // Use the API URL from the configuration
+        const apiUrl = config.apiUrl;
+        console.log(apiUrl); // Optional: Log the API URL for verification
 
-// Function to fetch YAML file
-async function fetchYaml(url) {
-    const response = await fetch(url);
-    const text = await response.text();
-    return yaml.safeLoad(text);
-}
+        function getDataFromAPI() {
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    displayData(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
 
-document.getElementById('getDataButton').addEventListener('click', async () => {
-    try {
-		// first test
-		//const msgContainer = document.getElementById('msg');
-		//msgContainer.innerHTML = `<p>You clicked</p>`;
-		alert('You clicked OK!');
-        // Fetch API URL from YAML configuration
-        /*const config = await fetchYaml('config.yaml');
-        const apiUrl = config.api_url;
-
-        // Fetch data from API
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        // Display data
-        displayData(data);*/
-    } catch (error) {
-        console.error('Error:', error);
-    }
-});
-
-function displayData(data) {
-    const resultContainer = document.getElementById('apiResult');
-    resultContainer.innerHTML = `
-        <p>User ID: ${data.userId}</p>
-        <p>Title: ${data.title}</p>
-        <p>Completed: ${data.completed ? 'Yes' : 'No'}</p>
-    `;
-}
+        function displayData(data) {
+            const resultContainer = document.getElementById('apiResult');
+            resultContainer.innerHTML = `
+                <p>User ID: ${data.userId}</p>
+                <p>Title: ${data.title}</p>
+                <p>Completed: ${data.completed ? 'Yes' : 'No'}</p>
+            `;
+        }
+    })
+    .catch(error => {
+        console.error('Error loading configuration:', error);
+    });
