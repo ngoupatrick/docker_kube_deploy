961216bb8962ed18a9af45eb4af73ade6d43c836
ngoupatrick <ngounepatrick@gmail.com>
2023-10-28 14:16:54 +0000

    update docker compose

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,11 @@
 document.getElementById('getDataButton').addEventListener('click', addPosts);
 
 function addPosts () {
+   //const api_key = process.env.api_key;
+   //const another_key = process.env.another_key;
+   
+
+
    fetch('http://127.0.0.1:5000/api/carre', {
    method: 'POST',
    body: JSON.stringify({
@@ -13,6 +18,8 @@ function addPosts () {
    .then((response) => response.json())
    .then((data) => {
       console.log(data);
+      //console.log('api_key'+api_key);
+      //console.log('another_key'+another_key);
    })
    .catch((err) => {
       console.log(err.message);
